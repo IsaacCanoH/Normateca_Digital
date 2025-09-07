bb0d8c10950e414d820d6b92774d29035bd7204a
IsaacCanoH <isac.c.2586@gmail.com>
2025-09-07 15:01:14 -0600

    Avance

--- normateca-digital/src/routes/Routes.jsx
+++ normateca-digital/src/routes/Routes.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NormatecaPage from "../pages/NormatecaPage";
 import LeyesPage from "../pages/LeyesPage";
+import ScrollToTop from "../components/ScrollToTop";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
+    <ScrollToTop />
       <Routes>
         <Route path="/" element={<NormatecaPage />} />
         <Route path="/leyes" element={<LeyesPage />} />
